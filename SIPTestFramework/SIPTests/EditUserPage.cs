231a9a3a64e3291cc105eb9e08d082f51673c012
kamarulhilmi <kamarulhilmi@gmail.com>
2017-06-02 16:53:17 +0800

    Update EditUserPage.cs

--- SIPTestFramework/SIPTests/EditUserPage.cs
+++ SIPTestFramework/SIPTests/EditUserPage.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SIPTests
 {
-    public class EditUserPage
+    public class EditUserPage : Browser
     {
         private static int PAGE_LOAD_TIMEOUT = 10;
 
